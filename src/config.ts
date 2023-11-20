caf025458e6d429351c76a8137c55cecb2f63005
nzdjb <github@2a1b.com>
2023-11-20 01:32:58 +0000

    refactor: Support array of config locations.

--- src/config.ts
+++ src/config.ts
@@ -7,14 +7,14 @@ import { globSync } from 'glob';
 export class Config {
   readonly articles: Article[];
 
-  constructor(configPath: string) {
-    const config = this.readConfig(configPath);
+  constructor(configPaths: string[]) {
+    const config = configPaths.flatMap((configPath) => this.readConfig(configPath)).join('\n');
     this.articles = (toml.parse(config).articles as unknown as ArticleConfig[]).map(article => new Article(article));
   }
 
-  private readConfig(configPath: string): string {
+  private readConfig(configPath: string): string[] {
     const configPathStat = lstatSync(configPath);
     const configFiles = configPathStat.isDirectory() ? globSync(path.join(configPath, '**', '*.toml')) : [configPath];
-    return configFiles.map(file => readFileSync(file).toString()).join('\n');
+    return configFiles.map(file => readFileSync(file).toString());
   }
 }
--- test/config.test.ts
+++ test/config.test.ts
@@ -3,7 +3,7 @@ import { Config } from '../src/config';
 
 describe('loadConfig', () => {
   test('single dir', () => {
-    const c = new Config('test/inputs');
+    const c = new Config(['test/inputs']);
     expect(c.articles).toIncludeSameMembers([
       new Article({
         author: 'Test',
@@ -23,7 +23,7 @@ describe('loadConfig', () => {
   });
 
   test('single file', () => {
-    const c = new Config('test/inputs/test.toml');
+    const c = new Config(['test/inputs/test.toml']);
     expect(c.articles).toEqual([
       new Article({
         author: 'Test',
@@ -31,6 +31,26 @@ describe('loadConfig', () => {
         date: new Date('2023-01-01'),
         published: true,
         title: 'Test',
+      }),
+    ]);
+  });
+
+  test('multiple files', () => {
+    const c = new Config(['test/inputs/test.toml', 'test/inputs/test2.toml']);
+    expect(c.articles).toIncludeSameMembers([
+      new Article({
+        author: 'Test',
+        content: '<p>Test</p>\n',
+        date: new Date('2023-01-01'),
+        published: true,
+        title: 'Test',
+      }),
+      new Article({
+        author: 'Test 2',
+        content: '<p>Test 2</p>\n',
+        date: new Date('2023-01-02'),
+        published: true,
+        title: 'Test 2',
       }),
     ]);
   });
